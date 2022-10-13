b76fcbe1c58837f84a2bf7d3c750a82e01b20fcd
Kiran19122001 <kiranban0011@gmail.com>
2022-10-13 23:02:42 +0530

    first commit

---
+++ src/startpattern/program.java
@@ -0,0 +1,20 @@
+package startpattern;
+public class program {
+	public static void main(String[] arg) 
+	{
+		int n=10;
+		int ch=65;
+		for(int i=1;i<=n;i++)
+		{
+			
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print((char)ch+" ");
+				
+			}ch++;
+			System.out.println();
+				
+		}
+				
+		}    		
+	}
